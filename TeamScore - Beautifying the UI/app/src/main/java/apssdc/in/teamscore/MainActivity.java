64a4848bba8e1eb4e9ff967b89cf0f8d2e2a8060
PAVAN KUMAR REDDY TADI <pavankumart46@gmail.com>
2020-08-20 15:27:56 +0530

    Beautifying the app

---
+++ TeamScore - Beautifying the UI/app/src/main/java/apssdc/in/teamscore/MainActivity.java
@@ -0,0 +1,32 @@
+package apssdc.in.teamscore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
+    private int score = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.score);
+    }
+
+    //for first button
+    public void decrementScore(View view) {
+        score--;
+        tv.setText(String.valueOf(score));
+    }
+
+    //for Second Button
+    public void incrementScore(View view) {
+        score++;
+        tv.setText(String.valueOf(score));
+    }
+}
